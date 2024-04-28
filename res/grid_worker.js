cde4cda6d1e1aae39dc00f83619b9745dad7aa36
Ashley <none>
2024-04-28 17:33:54 +0100

    remove grid_worker.js, tweak css

--- res/grid_worker.js
+++
@@ -1,8 +0,0 @@
-addEventListener("message", event => {
-  const [currentGrid, newGrid] = event.data;
-  var cellsToToggle = []
-    for (var i=0; i<currentGrid.length; i++){
-        cellsToToggle.push(currentGrid[i] != newGrid[i])
-    }
-  postMessage(cellsToToggle);
-});
